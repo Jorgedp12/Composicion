d99cf2eea9962ec53aee3f6001de89189aca47ef
Jorgedp12 <jediazp6805@gmail.com>
2026-09-22 14:28:26 -0500

    edicion

--- src/Asiento.java
+++ src/Asiento.java
@@ -1,34 +1,44 @@
 public class Asiento {
+
     private String codigo;
     private boolean ocupado;
+
 
     public Asiento(String codigo) {
         this.codigo = codigo;
         this.ocupado = false;
     }
 
+
     public String getCodigo() {
         return codigo;
     }
-    public boolean isOcupado(){
+
+    public boolean isOcupado() {
         return ocupado;
     }
 
-    public void ocupar(){
-        if(ocupado){
-            System.out.println("El asiento " + codigo + "esta ocupado.");
-        }else{
+
+    public void ocupar() {
+        if (ocupado) {
+            System.out.println("El asiento " + codigo + " ya está ocupado.");
+        } else {
             ocupado = true;
-            System.out.println("El asiento " + codigo + "fue asignado.");
+            System.out.println("Asiento " + codigo + " ocupado.");
         }
     }
 
-    public void liberar(){
-        ocupado = false;
-        System.out.println("Asiento " + codigo + "liberado.");
+    public void liberar() {
+        if (this.ocupado) {
+            ocupado = false;
+            System.out.println("Asiento " + codigo + " liberado.");
+        } else {
+            System.out.println("El Asiento no se encuentra ocupado, y no se puede liberar");
+        }
     }
 
-    public void mostrarEstado(){
+    public void mostrarEstado() {
         System.out.println("Asiento " + codigo + ": " + (ocupado ? "ocupado" : "libre"));
     }
+
 }
